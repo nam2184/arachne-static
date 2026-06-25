2c79c74f2e11c44c96c4d971426805f14a7e0fbb
nam2184 <namkhanh.cao123@gmail.com>
2026-06-25 14:04:17 +1000

    fix : base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isProjectPage = repoName && !repoName.endsWith(".github.io");
-
-const basePath =
-  process.env.NEXT_PUBLIC_BASE_PATH ??
-  (process.env.GITHUB_ACTIONS === "true" && isProjectPage ? `/${repoName}` : "");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
